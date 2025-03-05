168b3f6cd756ba1ff3dadd1479ed0fd013777ac5
Tobywhunt <tobywhunt@gmail.com>
2025-03-05 09:27:08 -0700

    1

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -73,38 +73,3 @@ export function Login() {
 
 
 
-
-
-
-
-
-
-// import React from 'react';
-// import "./login.css";
-
-
-
-// export function Login() {
-//   return (
-//     <main>
-//         <div className="area-2">
-//             <div className="middle">
-//                 <div className="welcome">
-//                     <p className="welcome-1">Welcome</p>
-//                 </div>
-//                 <div className="main-1">
-//                     <input className="email" type="email" placeholder="Email" />
-//                 </div>
-//                 <div className="main-2">
-//                     <input className="password" type="password" placeholder="Password" />
-//                 </div>
-//                 <div className="main-3">
-//                     <button className="login" type="submit">Login</button>
-//                     <button className="signup" type="submit">Sign Up</button>
-//                 </div>
-//             </div>
-//         </div>
-//     </main>
-
-//   );
-// }
